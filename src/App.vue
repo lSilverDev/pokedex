c43534431a605c533c2178162fce5b2fac5c6489
llSilverDev <67278780+lSilverDev@users.noreply.github.com>
2023-01-14 17:05:29 -0300

    pokemonCard

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
     <PokedexMenu/>
     <PokeSidebar/>
+    <PokemonCard/>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import PokeSidebar from "@/components/PokeSidebar/PokeSidebar.vue";
 import PokedexMenu from "@/components/PokedexMenu/PokedexMenu.vue";
+import PokemonCard from "@/components/PokemonCard/PokemonCard.vue";
 
 export default defineComponent({
   name: "App",
   components: {
     PokeSidebar,
-    PokedexMenu
+    PokedexMenu,
+    PokemonCard
   },
 });
 </script>
---
+++ src/components/PokemonCard/PokemonCard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="pokemonCard">
+        
+    </div>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+import axios from "axios";
+
+export default defineComponent({
+    name: "PokemonCard",
+    components:{
+    }
+})
+</script>
+
+<style>
+@import './style.css';
+</style>
