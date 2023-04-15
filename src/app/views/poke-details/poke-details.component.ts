e4d5265bec77a56a732d78050128d46622db6273
llSilverDev <67278780+lSilverDev@users.noreply.github.com>
2023-04-15 12:17:38 -0300

    Search details - removed

--- src/app/views/poke-details/poke-details.component.ts
+++ src/app/views/poke-details/poke-details.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
-import { map, switchMap } from 'rxjs';
 import { Pokemon } from 'src/app/models/pokemon';
 import { PokeServiceService } from 'src/app/service/poke-service.service';
 
@@ -12,18 +10,11 @@ import { PokeServiceService } from 'src/app/service/poke-service.service';
 })
 export class PokeDetailsComponent {
   pokemon: Pokemon;
-  searchPokemonDetail = new FormControl();
 
   constructor(private router: Router, private service: PokeServiceService) {
     const nav = this.router.getCurrentNavigation();
     this.pokemon = nav!.extras!.state!['pokemon'];
   }
-
-  pokemonsFinded$ = this.searchPokemonDetail.valueChanges
-  .pipe(
-    switchMap((search) => this.service.getPokemonByName(search)),
-    map(pokemon => this.pokemon = pokemon)
-  );
 
   return(){
     this.router.navigateByUrl("/");
